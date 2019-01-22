1f6b97e42bf8ccccc92f499eeea2f3fb91a00998
Alexander Ilchukov <alex.ilchukov@gmail.com>
2019-01-22 08:13:27 +0300

    #5 Add notifier's initialization

--- config/initializers/drate.rb
+++ config/initializers/drate.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 require "#{Rails.root}/lib/drate/actions/drate"
+require "#{Rails.root}/lib/drate/notifier"
+require "#{Rails.root}/lib/drate/streams"
+
+drate_path = "#{Rails.root}/data/drate"
 
 DRate::Actions::DRate.configure do |settings|
-  settings.path = ENV['DRATE_PATH'] || "#{Rails.root}/data/drate"
+  settings.path = drate_path
+end
+
+DRate::Notifier.instance.on_close_write(drate_path, :drate) do
+  DRate::Streams.instance.broadcast(DRate::Actions::Drate.show)
 end
