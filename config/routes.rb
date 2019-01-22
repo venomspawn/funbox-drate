8336168bc900c37d371cfaf05f68add3992bd547
Alexander Ilchukov <alex.ilchukov@gmail.com>
2019-01-22 10:06:27 +0300

    #5 Add support of SSE on dollar's rate changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@
 
 Rails.application.routes.draw do
   root to: 'default/page#draw'
+
+  get '/stream', to: 'default/stream#stream'
 end
--- spec/requests/default_spec.rb
+++ spec/requests/default_spec.rb
@@ -21,4 +21,57 @@ RSpec.describe 'Default access API' do
       end
     end
   end
+
+  describe 'GET /stream', type: :request do
+    before do
+      DRate::Actions::DRate.settings.path = path
+      stub_const('Default::StreamController::DELAY', delay)
+      DRate::Streams.instance.reset
+      allow(DRate::Streams.instance)
+        .to receive(:include?)
+        .and_return(false, true, false)
+      original_register = DRate::Streams.instance.method(:register)
+      allow(DRate::Streams.instance).to receive(:register) do |stream|
+        original_register[stream]
+        DRate::Streams.instance.broadcast(rate)
+      end
+      get '/stream'
+    end
+
+    subject { response }
+
+    let(:delay) { 0.1 }
+    let(:rate) { DRate::Actions::DRate.show }
+    let(:path) { "#{Rails.root}/spec/fixtures/data/drate" }
+
+    describe 'headers' do
+      subject { response.headers }
+
+      it { is_expected.to include('Content-Type') }
+
+      describe '`Content-Type` value' do
+        subject { response.headers['Content-Type'] }
+
+        it { is_expected.to be == 'text/event-stream' }
+      end
+    end
+
+    describe 'output stream buffer' do
+      subject { response.stream.instance_variable_get(:@buf) }
+
+      context 'when nothing happens' do
+        let(:ping) { Default::StreamController::PING_MESSAGE }
+
+        it 'should have ping' do
+          expect(subject.first).to include(ping)
+        end
+      end
+
+      context 'when content of the file with dollar\'s rate changes' do
+        it 'should have the rate' do
+          expect(subject.first).to include(rate)
+        end
+      end
+    end
+  end
 end
